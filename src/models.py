001a2ab0e090364f0e63b46f2115c08ad40b2856
moniasuka <moniasuka@hotmail.com>
2022-10-19 03:01:50 +0000

    star wars database

--- src/models.py
+++ src/models.py
@@ -29,5 +29,59 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table characters.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    url = Column(String(250))
+    height = Column(Integer)
+    mass = Column(Integer)
+    hair_color  = Column(String(250))
+    skin_color  = Column(String(250))
+    eye_color  = Column(String(250))
+    birth_year = Column(Integer)
+    gender = Column(String(250))
+    homeworld = Column(String(250))
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table planets.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    url = Column(String(250))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    population = Column(Integer)
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(Integer)
+
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table vehicles.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    url = Column(String(250))
+    model = Column(String(250))
+    vehicle_class = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(Integer)
+    length = Column(Integer)
+    crew = Column(Integer)
+    passengers = Column(Integer)
+    max_atmosphering_speed = Column(Integer)
+    cargo_capacity = Column(Integer)
+    consumables = Column(String(250))
+    films = Column(String(250))
+    pilots = Column(String(250))
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
